a8ab8484232c4905253fab4be4b5159c58e713d6
Michael Wautier <wautiermichael@gmail.com>
2020-12-20 13:27:00 +0100

    list displayed

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,4 +1,13 @@
 import "../css/main.scss";
 import collections from "../../app/collections"
 
-console.log(collections)
+const collectionsList = document.getElementById("collections-list")
+
+collections.forEach(collection => {
+  const listItem = document.createElement('li')
+  listItem.innerHTML = `<a href="#">
+                          /${collection.title} * <span>${collection.category}</span>
+                        </a>`
+  listItem.classList.add("list-item")
+  collectionsList.appendChild(listItem)
+});
